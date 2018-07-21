59702af29fe1d7e565b9ef31a473bd9aecf96149
scthompson <scthompson@users.noreply.github.com>
2018-07-21 18:11:56 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <HeaderMain />
+    <ContentMain />
+  </div>
+</template>
+
+<script>
+import HeaderMain from './components/headerMain.vue'
+import ContentMain from './components/contentMain.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HeaderMain,
+    ContentMain
+  }
+}
+</script>
+
+<style>
+
+  body {
+    margin:0;
+  }
+</style>
